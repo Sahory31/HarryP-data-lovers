5551971f2898b986808040c24d9ab8a064f4a85a
JossAcosta <josselineacostamtz@gmail.com>
2019-11-13 12:26:58 -0600

    vanilla version

--- src/main.js
+++ src/main.js
@@ -1,15 +1,31 @@
-/**
+/*
  * import POKEMON from './data/pokemon/pokemon.js'
  * import LoL from './data/lol/lol.js'
  * import POTTER from './data/potter/potter.js'
  */
 
-import { example } from './data.js';
+/*import { example } from './data.js';
 
-console.log(example);
+console.log(example); */
 
 /*
  * console.log(POKEMON);
  * console.log(LoL);
  * console.log(POTTER)
 */
+
+var timer = null;
+var page = document.getElementById('page');
+window.onscroll = function () {
+  if (timer) {
+    clearTimeout(timer);
+    timer = null;
+  }
+  else {
+    page.classList.toggle('scrolling');
+  }
+  timer = setTimeout(function () {
+    page.classList.toggle('scrolling');
+    timer = null;
+  }, 150);
+}
