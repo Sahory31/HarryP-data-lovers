dec0bc2d5e4d33cb83c87767030c982d7430ffb1
JossAcosta <josselineacostamtz@gmail.com>
2019-11-20 13:13:17 -0600

    titles in each categories

--- src/main.js
+++ src/main.js
@@ -25,10 +25,16 @@ const page_Ravenclaw =document.getElementById('page_Ravenclaw');
 const page_slytherin =document.getElementById('page_slytherin');
 const back = document.getElementById('back');
 
+const arrTitle = ['Bienvenidos','Todos', 'Mujeres','Hombres','Griffindor','Hufflepuff', 'Slytherin', 'Ravenclaw'];
+const title = document.getElementById("title");
+title.innerHTML=`${arrTitle[0]}`;
+
 page_all.addEventListener('click', event=> {
+  
    page.classList.add('hide');
    categoryAll.classList.remove('hide');
    createTemplateCard(harryData);
+   title.innerHTML=`${arrTitle[1]}`;
   });
 
 page_girls.addEventListener('click', event=> {
@@ -36,43 +42,47 @@ page_girls.addEventListener('click', event=> {
     page.classList.add('hide');
     categoryAll.classList.remove('hide');
     createTemplateCard(filterGirlsNames);
+    title.innerHTML=`${arrTitle[2]}`;
    });
   page_men.addEventListener('click', event=> {
    // alert ("men");
     page.classList.add('hide');
     categoryAll.classList.remove('hide');
     createTemplateCard(filterMenNames);
+    title.innerHTML=`${arrTitle[3]}`;
     });
    page_griffindor.addEventListener('click', event=> {
    // alert ("griffindor");
     page.classList.add('hide');
     categoryAll.classList.remove('hide');
     createTemplateCard(filterGriffindor);
+    title.innerHTML=`${arrTitle[4]}`;
    });
    page_Hufflepuff.addEventListener('click', event=> {
     //alert ("hufflepuff");
     page.classList.add('hide');
     categoryAll.classList.remove('hide');
     createTemplateCard(filterhufflepuff);
-  
+    title.innerHTML=`${arrTitle[5]}`;
    });
    page_Ravenclaw.addEventListener('click', event=> {
    // alert ("ravenclaw");
     page.classList.add('hide');
     categoryAll.classList.remove('hide');
     createTemplateCard(filterRavenclaw);
-  
+    title.innerHTML=`${arrTitle[5]}`;
    });
    page_slytherin.addEventListener('click', event=> {
     //alert ("slytherin");
     page.classList.add('hide');
     categoryAll.classList.remove('hide');
     createTemplateCard(filterslytherin);
+    title.innerHTML=`${arrTitle[6]}`;
      });
      back.addEventListener('click', event=> {
-      
       page.classList.remove('hide');
       categoryAll.classList.add('hide');
+      title.innerHTML=`${arrTitle[0]}`;
       
        });
 
@@ -88,6 +98,8 @@ const filterGriffindor = app.gryffindorHouseCategory(POTTER);
 const filterhufflepuff = app.hufflepuffHouseCategory(POTTER);
 const filterslytherin = app.slytherinHouseCategory(POTTER);
 const filterRavenclaw = app.RavenclawHouseCategory(POTTER);
+
+
 
 const createTemplateCard = list =>{
   let templateCard ='';
